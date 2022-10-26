53f303e684278028a25882e100a228255dd426a7
Barbe_White <benoit.dropek@hotmail.fr>
2022-10-26 16:43:40 +0200

    Implémentation des monstres

---
+++ src/Dragon.java
@@ -0,0 +1,22 @@
+public class Dragon {
+    private int vie;
+    private int force;
+    private String faiblesse;
+
+
+    public int getVie() {
+        return vie;
+    }
+
+    public void setVie(int vie) {
+        this.vie = vie;
+    }
+
+    public int getForce() {
+        return force;
+    }
+
+    public String getFaiblesse() {
+        return faiblesse;
+    }
+}
